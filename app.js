4d805efaa967f1234f83c4f4317fe79d84402c44
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-16 12:31:39 +0300

    add incidents router to server

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express  from "express"
 import operatorRouter from "./routes/operatorRouter.js"
+import incidentRouter from "./routes/incidentRouter.js"
 
 const app = express()
 
 app.use(express.json())
 
 app.use("/operator", operatorRouter)
+
+app.use("/incidents", incidentRouter)
 
---
+++ routes/incidentRouter.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { createIncident, changeStatus, getAllIncidents } from "../controllers/incidentController.js"
+
+
+const router = express.Router()
+
+router.post("", createIncident)
+
+router.patch("/:id/status", changeStatus)
+
+router.get("/open", getAllIncidents)
+
+export default router
